779de3aa13ef468d63f6f786c5935c59edaacf95
Olivier Morlotti <o.morlotti@gmail.com>
2021-01-28 15:11:13 +0100

    Config SpringBoot

---
+++ WebAPI/src/main/java/xyz/morlotti/virtualbookcase/webapi/VirtualBookcaseWebApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.morlotti.virtualbookcase.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBookcaseWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualBookcaseWebApiApplication.class, args);
+	}
+
+}
