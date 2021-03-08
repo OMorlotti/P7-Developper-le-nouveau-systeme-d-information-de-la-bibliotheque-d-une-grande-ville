9c1b6311e5e20082485437b5a600ef1bece9f89e
OMorlotti <o.morlotti@gmail.com>
2021-03-08 14:29:42 +0100

    POMs

--- WebAPI/src/main/java/xyz/morlotti/virtualbookcase/webapi/security/jwt/AuthTokenFilter.java
+++ WebAPI/src/main/java/xyz/morlotti/virtualbookcase/webapi/security/jwt/AuthTokenFilter.java
@@ -52,7 +52,7 @@ public class AuthTokenFilter extends OncePerRequestFilter
 		}
 		catch(Exception e)
 		{
-			logger.error("Cannot set user authentication: {}", e);
+			logger.error("Cannot set user authentication: {}", e.getMessage());
 		}
 
 		filterChain.doFilter(request, response);
